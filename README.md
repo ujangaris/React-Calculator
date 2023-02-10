67c5695f324be6e1a6c739ca97af98d25ea0af79
ujangaja <ujangaja@gmail.com>
2023-02-10 07:29:40 +0700

    Hook pada functional component

--- README.md
+++ README.md
@@ -7,3 +7,13 @@
             setup html
         2. index.css
             styling css
+
+## Hook pada functional component
+
+    Todo:
+        1. App.js
+           - Buat Hook calculation dan result
+           - Memberi nilai pada button operator & digit, dan mencetak nilai dari digit
+           - createDigits()
+           - updateCalc()
+             mencetak nilai dari digit setelah dilakukan ('*','/','+','-')
--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
-// import "./App.css"
+import { useState } from "react"
 
 function App() {
+  // Hook calculation dan result
+  const [calc, setCalc] = useState("")
+  const [result, setResult] = useState("")
+
+  const ops = ["/", "*", "+", "-", "."]
+
+  const updateCalc = (value) => {
+    if (
+      (ops.includes(value) && calc === "") ||
+      (ops.includes(value) && ops.includes(calc.slice(-1)))
+    ) {
+      return
+    }
+    setCalc(calc + value)
+    if (!ops.includes(value)) {
+      setResult(eval(calc + value).toString())
+    }
+  }
+
   const createDigits = () => {
     const digits = []
     for (let i = 1; i < 10; i++) {
-      digits.push(<button key={i}> {i} </button>)
+      digits.push(
+        <button onClick={() => updateCalc(i.toString())} key={i}>
+          {i}{" "}
+        </button>
+      )
     }
     return digits
   }
@@ -12,16 +35,23 @@ function App() {
     <div className="App">
       <div className="calculator">
         <div className="display">
-          <span> (0) </span>0{" "}
+          {result ? <span>{result} </span> : ""}
+          {calc || "0"}{" "}
         </div>{" "}
+        {/* Memberi nilai pada button operator  */}
         <div className="operators">
-          <button> /</button>
-          <button> * </button> <button> + </button> <button> - </button>{" "}
+          <button onClick={() => updateCalc("/")}> /</button>
+          <button onClick={() => updateCalc("*")}> * </button>{" "}
+          <button onClick={() => updateCalc("+")}> + </button>{" "}
+          <button onClick={() => updateCalc("-")}> - </button>{" "}
           <button> DEL </button>{" "}
         </div>{" "}
+        {/* Memberi nilai pada button  digit */}
         <div className="digits">
           {" "}
-          {createDigits()} <button> 0 </button> <button> . </button>{" "}
+          {createDigits()}
+          <button onClick={() => updateCalc("0")}> 0 </button>{" "}
+          <button onClick={() => updateCalc(".")}> . </button>{" "}
           <button>= </button>{" "}
         </div>{" "}
       </div>{" "}
